e35a77289ffbc443537c97d6432bdb686c8ac877
Steven Gutierrez <stevogbmv@gmail.com>
2022-11-24 14:57:33 -0800

    commit tree 709938983d9449c63d11cb7039b2541ea7b9e08f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName (){
+    return customerName = customerName.toUpperCase();
+}
+function setBestCustomer (){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer (){
+    return bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'Tom'
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "Riddle";
+}
